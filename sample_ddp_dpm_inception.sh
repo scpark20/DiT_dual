7e64f4e0e7cb9f25f42a456c81f46a2211b507a3
scpark <scpark20@gmail.com>
2025-08-31 08:54:18 +0900

    push

--- sample_ddp_dpm_inception.sh
+++ sample_ddp_dpm_inception.sh
@@ -1,4 +1,4 @@
-torchrun --nnodes=1 --nproc_per_node=2 sample_ddp_dpm_inception.py --model DiT-XL/2 --num-fid-samples 50000 --n_steps 3
-torchrun --nnodes=1 --nproc_per_node=2 sample_ddp_dpm_inception.py --model DiT-XL/2 --num-fid-samples 50000 --n_steps 5
-torchrun --nnodes=1 --nproc_per_node=2 sample_ddp_dpm_inception.py --model DiT-XL/2 --num-fid-samples 50000 --n_steps 7
-torchrun --nnodes=1 --nproc_per_node=2 sample_ddp_dpm_inception.py --model DiT-XL/2 --num-fid-samples 50000 --n_steps 9
+torchrun --nnodes=1 --nproc_per_node=8 sample_ddp_dpm_inception.py --model DiT-XL/2 --num-fid-samples 50000 --n_steps 3
+torchrun --nnodes=1 --nproc_per_node=8 sample_ddp_dpm_inception.py --model DiT-XL/2 --num-fid-samples 50000 --n_steps 5
+torchrun --nnodes=1 --nproc_per_node=8 sample_ddp_dpm_inception.py --model DiT-XL/2 --num-fid-samples 50000 --n_steps 7
+torchrun --nnodes=1 --nproc_per_node=8 sample_ddp_dpm_inception.py --model DiT-XL/2 --num-fid-samples 50000 --n_steps 9
